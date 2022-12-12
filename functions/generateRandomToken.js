3359d5ce978dd292f4ba6ad0f333ff8045bdd292
devdriven7 <120395991+devdriven7@users.noreply.github.com>
2022-12-12 17:33:00 +0000

    Integrated randExp lib

--- functions/generateRandomToken.js
+++ functions/generateRandomToken.js
@@ -1,6 +1,8 @@
+const randExp = require('randexp').randexp;
+
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'New Random Token' })
+        body: JSON.stringify({ token: randExp(/<([a-z]\w{0,20})>foo<\1>/) })
     }
 }
